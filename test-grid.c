96e3d68d496e232ee5fcfdaa1c8c061056737a65
Gaetan_Chambres <chambres.gaetan@gmail.com>
2015-02-14 17:44:58 +0100

    ajout d'un scan écran de l'affichage version 3 et mise a jour du fichier test-grid.c

--- test-grid.c
+++ test-grid.c
@@ -4,28 +4,39 @@
 #include <stdio.h>
 
 //fonction pour tester notre grille et son affichage
-//i represente les colones et j les lignes
+//i représente les colonnes et j les lignes (commentaire à corriger)
 
-void afficher (grid g){
-  printf (" +~~~~~~+~~~~~~~+~~~~~~~+~~~~~~~+ \n ");  
-  for (int i=0; i<GRID_SIDE; ++i){
-	for (int j=0; j<GRID_SIDE; ++j){
-		tile res=(get_tile (g,i,j));
-		if (res==0){
-			printf ("|      |");
-			}
-		else{
-	    		printf ("| %4d |",res);
-			}
+void afficher (grid g) {
+	
+	printf (" +~~~~+~~~~+~~~~+~~~~+ \n ");  
+	
+	for (int i=0; i<GRID_SIDE; ++i) {
+		printf("|");
+		for (int j=0; j<GRID_SIDE; ++j) {
+			tile res = (get_tile (g,j,i));
+			if (res == 0)
+				printf ("    |");
+			else
+				printf ("%4d|",res);
 		}
-	printf ("\n +~~~~~~+~~~~~~~+~~~~~~~+~~~~~~~+ \n ");
-	}	
-  unsigned long int score=(grid_score (g));
-  printf ("votre score est de %d \n",score);
+		printf ("\n +~~~~+~~~~+~~~~+~~~~+ \n ");
+	}
+	
+	printf ("Votre score est de %lu points\n", grid_score(g));	
 }
+  
 
-int main (void){
-  grid g=new_grid ();
-  afficher(g);  
-  return EXIT_SUCCESS;
+int main (void) {
+	
+	grid g = new_grid ();
+	
+	for (int i=0; i<GRID_SIDE; ++i)
+		for (int j=0; j<GRID_SIDE; ++j)
+			set_tile(g,j,i,2);
+			
+	afficher(g);
+	do_move(g, RIGHT);
+	afficher(g);
+	 
+	return EXIT_SUCCESS;
 }
