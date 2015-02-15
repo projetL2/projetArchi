3e00c120911e3fd094b868ff3f880dd4ce3f44d1
Galmyre <zazarlaliche@gmail.com>
2015-02-15 16:36:07 +0100

    Quand vous avez fait le dépot, vous avez pas pris la dernière version du codeshare, il manquait un bout de do_move du coup, je l'ai rajouté et j'ai fait un coup de astyle sur les .c pour l'incrémentation, fonction add_tile en cours

--- grid.c
+++ grid.c
@@ -2,31 +2,32 @@
 #include "grid.h"
 #include <stdlib.h>
 #include <assert.h>
+#include <time.h>
 
 // La structure de la grille, un pointeur de pointeur qui nous donnera la matrice
 // et une unsigned long int score
 struct grid_s {
-	unsigned long int score;
-	tile** grid;
+    unsigned long int score;
+    tile** grid;
 };
 
 //Le constructeur, on initialise le score à 0
 //et on fait des malloc pour la struct et la grille
 grid new_grid () {
 
-	grid g = malloc(sizeof(struct grid_s));
-	assert (g!=NULL);
+    grid g = malloc(sizeof(struct grid_s));
+    assert (g!=NULL);
 
-	g->score=0;
-	g->grid = malloc(GRID_SIDE * sizeof(tile*));
+    g->score=0;
+    g->grid = malloc(GRID_SIDE * sizeof(tile*));
     assert (g->grid!=NULL);
 
-	for (int i=0; i<GRID_SIDE; ++i) {
+    for (int i=0; i<GRID_SIDE; ++i) {
         g->grid[i] = malloc (GRID_SIDE * sizeof(tile));
         assert (g->grid[i]!=NULL);
-	}
-  
-  return g;
+    }
+
+    return g;
 }
 
 //Destructeur de la grille, on libère d'abord les zones mémoire pointées par la variable grid puis la grille elle-même
@@ -40,13 +41,13 @@ void delete_grid (grid g) {
 
 //Constructeur par copie de grid
 void copy_grid (grid src, grid dst) {
- 
-	dst->score = src->score;
-  	
-  	for (int i=0; i<GRID_SIDE; ++i)
-      	for (int j=0; j<GRID_SIDE; ++j)
-      		dst->grid[i][j] = src->grid[i][j];
-      
+
+    dst->score = src->score;
+
+    for (int i=0; i<GRID_SIDE; ++i)
+        for (int j=0; j<GRID_SIDE; ++j)
+            dst->grid[i][j] = src->grid[i][j];
+
 }
 
 //Accesseur qui retourne le score
@@ -60,8 +61,8 @@ tile get_tile (grid g, int x, int y) {
 
     if ((x>=0 && x<=GRID_SIDE) && (y>=0 && y<=GRID_SIDE))
         return g->grid[x][y];
-  	else 
-    	return -1; // solution temporaire pour avoir toujours un return 
+    else
+        return -1; // solution temporaire pour avoir toujours un return
 }
 
 //Accesseur qui instancie la valeur d'une tile en fonction des coordonnées et de la valeur en paramètres
@@ -72,124 +73,139 @@ void set_tile (grid g, int x, int y, tile t) {
 
 //Fonction booléene qui renvoie true si le joueur peut effectuer un movement dans une direction
 bool can_move (grid g, dir d) {
-  
- 	 // on vérifie la direction demandée (d)
- 	 // suivant cette dernière, on parcourt la grille pour voir si on peut déplacer ou fusionner, 
-  	 //si oui, on renvoie true
-  	switch (d) {
-    
-      case LEFT:
-          for (int i=1; i<GRID_SIDE; ++i)  //i represente les colones
-              for (int j=0; j<GRID_SIDE; ++j) //j represente les lignes
-                  if ( (g->grid[i-1][j] == 0) || (g->grid[i][j] == g->grid[i-1][j]) ) // si case suivante vide ou fusion possible
-                      return true;	
-      break;
 
-      case RIGHT:
-          for (int i=0; i<GRID_SIDE-1; ++i)
-              for (int j=0; j<GRID_SIDE; ++j)	
-                  if ( (g->grid[i+1][j] == 0) || (g->grid[i][j] == g->grid[i+1][j]) )
-                 	  return true;
-      break;
+    // on vérifie la direction demandée (d)
+    // suivant cette dernière, on parcourt la grille pour voir si on peut déplacer ou fusionner,
+    //si oui, on renvoie true
+    switch (d) {
 
-      case UP:
-          for (int i=0; i<GRID_SIDE; ++i)
-              for (int j=1; j<GRID_SIDE; ++j)	
-                  if ( (g->grid[i][j-1] == 0) || (g->grid[i][j-1] == g->grid[i][j]) )
-                      return true;
-      break;
+    case LEFT:
+        for (int i=1; i<GRID_SIDE; ++i)  //i represente les colones
+            for (int j=0; j<GRID_SIDE; ++j) //j represente les lignes
+                if ( (g->grid[i-1][j] == 0) || (g->grid[i][j] == g->grid[i-1][j]) ) // si case suivante vide ou fusion possible
+                    return true;
+        break;
 
-      case DOWN:
-          for (int i=0; i<GRID_SIDE; ++i)
-              for (int j=0; j<GRID_SIDE-1; ++j)	
-                  if ( (g->grid[i][j+1] == 0) || (g->grid[i][j+1] == g->grid[i][j]) )
-                      return true;
-      break;
-	}
-  // Si le mouvement demandé est impossible, on renvoie false
-  return false;
+    case RIGHT:
+        for (int i=0; i<GRID_SIDE-1; ++i)
+            for (int j=0; j<GRID_SIDE; ++j)
+                if ( (g->grid[i+1][j] == 0) || (g->grid[i][j] == g->grid[i+1][j]) )
+                    return true;
+        break;
+
+    case UP:
+        for (int i=0; i<GRID_SIDE; ++i)
+            for (int j=1; j<GRID_SIDE; ++j)
+                if ( (g->grid[i][j-1] == 0) || (g->grid[i][j-1] == g->grid[i][j]) )
+                    return true;
+        break;
+
+    case DOWN:
+        for (int i=0; i<GRID_SIDE; ++i)
+            for (int j=0; j<GRID_SIDE-1; ++j)
+                if ( (g->grid[i][j+1] == 0) || (g->grid[i][j+1] == g->grid[i][j]) )
+                    return true;
+        break;
+    }
+    // Si le mouvement demandé est impossible, on renvoie false
+    return false;
 }
 
 //Fonction booléenne qui renvoie TRUE si aucun mouvement n'est plus possible.
-bool game_over(grid g){
-  
-  if ( !can_move(g,UP) &&
-       !can_move(g,DOWN) &&
-       !can_move(g,LEFT) &&
-       !can_move(g,RIGHT))
-    return true;
-  
-  else
-    return false;
+bool game_over(grid g) {
+
+    if ( !can_move(g,UP) &&
+            !can_move(g,DOWN) &&
+            !can_move(g,LEFT) &&
+            !can_move(g,RIGHT))
+        return true;
+
+    else
+        return false;
 }
 
 //Fonction qui déplace les tiles de la grid si le mouvement est possible
 void do_move (grid g, dir d) {
-  
-  if (can_move(g,d)) {
-    // dans tout le switch, on utilisera i pour parcourir les colonnes et j pour les lignes
-   	 switch (d) {
-    
-      case LEFT:
-          for (int j = 0; j<GRID_SIDE; ++j)	// on parcourt de gauche à droite
-            for (int i = 1; j<GRID_SIDE; ++i)   // et de haut en bas
-            	if (g->grid[i][j] != 0) // si la case n'est pas vide
-            		if (g->grid[i-1][j] == 0) {  // si la case de gauche est vide, on décale la case actuelle
-          				g->grid[i-1][j] = g->grid[i][j];
-            			g->grid[i][j] = 0;
-            			--i; // on décrémente i au cas où il y ait à déplacer la tile plusieurs fois
-          			}
-       			    else if (g->grid[i][j] == g->grid[i-1][j]) { // s'il doit y avoir une fusion
-         				g->grid[i][j] = 0;
-                      	g->grid[i-1][j] *= 2;				// on vide la case actuelle et on double la valeur de celle qu'elle a heurté
-                      	g->score += g->grid[i-1][j];		// on actualise le score
-                    }
-            	            
-      break;
 
-      case RIGHT:
-          for (int j=0; j<GRID_SIDE; ++j) // on parcourt de droite à gauche
-            for (int i = GRID_SIDE-2; i>=0; --i) // et haut en bas
-            	if (g->grid[i][j] != 0) // si la case n'est pas vide
-            		if (g->grid[i+1][j] == 0) {  // si la case de droite est vide, on décale la case actuelle
-          				g->grid[i+1][j] = g->grid[i][j];
-            			g->grid[i][j] = 0;
-            			++i; // on incrémente i au cas où il y ait à déplacer la tile plusieurs fois
-          			}
-       			    else if (g->grid[i][j] == g->grid[i+1][j]) { // s' il doit y avoir une fusion
-         				g->grid[i][j] = 0;
-                      	g->grid[i+1][j] *= 2;				// on vide la case actuelle et on double la valeur de celle qu'elle a heurté
-                      	g->score += g->grid[i+1][j];		// on actualise le score
-                    }
-             
-      break;
-/*
-      case UP:
-          for (int i=0; i<GRID_SIDE; ++i)
-              for (int j=1; j<GRID_SIDE; ++j)	
-                  if ( (g->grid[i][j-1] == 0) || (g->grid[i][j-1] == g->grid[i][j]) ) // si case suivante vide ou fusion possible
-                      return true;
-      break;
+    if (can_move(g,d)) {
+        // dans tout le switch, on utilisera i pour parcourir les colonnes et j pour les lignes
+        switch (d) {
 
-      case DOWN:
-          for (int i=0; i<GRID_SIDE; ++i)
-              for (int j=0; j<GRID_SIDE-1; ++j)	
-                  if ( (g->grid[i][j+1] == 0) || (g->grid[i][j+1] == g->grid[i][j]) ) // si case suivante vide ou fusion possible
-                      return true;
-      break;
-	}*/
-  }
- }
+        case LEFT:
+            for (int ligne = 0; ligne<GRID_SIDE; ++ligne)	// on parcourt de gauche à droite
+                for (int colonne = 1; colonne<GRID_SIDE; ++colonne) {   // et de haut en bas
+                    if (g->grid[colonne][ligne] != 0) { // si la case n'est pas vide
+                        if (g->grid[colonne-1][ligne] == 0) {  // si la case de gauche est vide, on décale la case actuelle
+                            g->grid[colonne-1][ligne] = g->grid[colonne][ligne];
+                            g->grid[colonne][ligne] = 0;
+                            colonne-=2; // on décrémente i au cas où il y ait à déplacer la tile plusieurs fois
+                        }
+                        else if (g->grid[colonne][ligne] == g->grid[colonne-1][ligne]) { // s'il doit y avoir une fusion
+                            g->grid[colonne][ligne] = 0;
+                            g->grid[colonne-1][ligne] *= 2;				// on vide la case actuelle et on double la valeur de celle qu'elle a heurté
+                            g->score += g->grid[colonne-1][ligne];		// on actualise le score
+                        }
+                    }
+                }
+
+            break;
+
+        case RIGHT:
+            for (int ligne=0; ligne<GRID_SIDE; ++ligne) {// on parcourt de droite à gauche
+                for (int colonne = GRID_SIDE-2; colonne>=0; --colonne) { // et haut en bas
+                    if (g->grid[colonne][ligne] != 0 && colonne < 3) { // si la case n'est pas vide
+                        if (g->grid[colonne+1][ligne] == 0) {  // si la case de droite est vide, on décale la case actuelle
+                            g->grid[colonne+1][ligne] = g->grid[colonne][ligne];
+                            g->grid[colonne][ligne] = 0;
+                            colonne+=2; // on incrémente i au cas où il y ait à déplacer la tile plusieurs fois
+                        }
+                        else if (g->grid[colonne][ligne] == g->grid[colonne+1][ligne]) { // s' il doit y avoir une fusion
+                            g->grid[colonne][ligne] = 0;
+                            g->grid[colonne+1][ligne] *= 2;			// on vide la case actuelle et on double la valeur de celle qu'elle a heurté
+                            g->score += g->grid[colonne+1][ligne];		// on actualise le score
+                        }
+                    }
+                }
+            }
+            break;
+
+        case DOWN:
+            for (int colonne=0; colonne<GRID_SIDE; ++colonne) // on parcourt de gauche à droite
+                for (int ligne = GRID_SIDE-2; ligne>=0; --ligne) // de bas en haut
+                    if (g->grid[colonne][ligne] != 0 && ligne < 3) {		// si la case n'est pas vide
+                        if (g->grid[colonne][ligne+1] == 0) {		// si la case en dessous d'elle est vide, on décale
+                            g->grid[colonne][ligne+1] = g->grid[colonne][ligne];
+                            g->grid[colonne][ligne] = 0;
+                            ligne+=2; // on incrémente les lignes au cas ou on doive déplacer plusieurs fois la tile
+                        }
+                        else if (g->grid[colonne][ligne+1] == g->grid[colonne][ligne]) {
+                            g->grid[colonne][ligne] = 0;
+                            g->grid[colonne][ligne+1] *= 2;
+                            g->score += g->grid[colonne][ligne+1];
+                        }
+                    }
+            break;
+            /*
+                  case DOWN:
+                      for (int i=0; i<GRID_SIDE; ++i)
+                          for (int j=0; j<GRID_SIDE-1; ++j)
+                              if ( (g->grid[i][j+1] == 0) || (g->grid[i][j+1] == g->grid[i][j]) ) // si case suivante vide ou fusion possible
+                                  return true;
+                  break;
+            	}*/
+        }
+    }
 }
 
 void add_tile (grid g) {
-  
+
+    rand()%GRID_SIDE;
 }
 
 void play (grid g, dir d) {
-	
-  	if(can_move(g,d)) {
-      	do_move(g,d);
-		add_tile;
-	}
+
+    if(can_move(g,d)) {
+        do_move(g,d);
+        add_tile;
+    }
 }
--- test-grid.c
+++ test-grid.c
@@ -2,41 +2,39 @@
 #include "grid.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 //fonction pour tester notre grille et son affichage
 //i représente les colonnes et j les lignes (commentaire à corriger)
 
 void afficher (grid g) {
-	
-	printf (" +~~~~+~~~~+~~~~+~~~~+ \n ");  
-	
-	for (int i=0; i<GRID_SIDE; ++i) {
-		printf("|");
-		for (int j=0; j<GRID_SIDE; ++j) {
-			tile res = (get_tile (g,j,i));
-			if (res == 0)
-				printf ("    |");
-			else
-				printf ("%4d|",res);
-		}
-		printf ("\n +~~~~+~~~~+~~~~+~~~~+ \n ");
-	}
-	
-	printf ("Votre score est de %lu points\n", grid_score(g));	
+
+    printf (" +~~~~+~~~~+~~~~+~~~~+ \n ");
+
+    for (int i=0; i<GRID_SIDE; ++i) {
+        printf("|");
+        for (int j=0; j<GRID_SIDE; ++j) {
+            tile res = (get_tile (g,j,i));
+            if (res == 0)
+                printf ("    |");
+            else
+                printf ("%4d|",res);
+        }
+        printf ("\n +~~~~+~~~~+~~~~+~~~~+ \n ");
+    }
+
+    printf ("Votre score est de %lu points\n", grid_score(g));
 }
-  
+
 
 int main (void) {
-	
-	grid g = new_grid ();
-	
-	for (int i=0; i<GRID_SIDE; ++i)
-		for (int j=0; j<GRID_SIDE; ++j)
-			set_tile(g,j,i,2);
-			
-	afficher(g);
-	do_move(g, RIGHT);
-	afficher(g);
-	 
-	return EXIT_SUCCESS;
+
+    grid g = new_grid ();
+
+
+    srand(time (NULL)); // initialisation de rand
+    afficher(g);
+
+
+    return EXIT_SUCCESS;
 }
